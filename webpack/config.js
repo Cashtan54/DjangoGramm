bc2fad2b64ee6422b7e5d1fca05d95ec90d3a144
asus <romantsovmil.comitya1@gma>
2022-07-29 11:53:04 +0300

    webpack 29.07

--- webpack/config.js
+++ webpack/config.js
@@ -1,7 +1,8 @@
 var webpack = require("webpack");
 var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+config_js = {
     entry: [
         './src/js/jquery_follow.js',
         './src/js/jquery_func.js',
@@ -10,7 +11,7 @@ module.exports = {
     mode: 'development',
     output: {
         filename: 'index.js',
-        path: __dirname
+        path: path.resolve(__dirname, '../djangogramm/static/djangogramm/js/')
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -19,3 +20,25 @@ module.exports = {
         })
     ]
 }
+
+config_css = {
+  entry: './src/css/main_css.js',
+  mode: 'development',
+  output: {
+        filename: 'main.css',
+        path: path.resolve(__dirname, '../djangogramm/static/djangogramm/css/')
+    },
+  plugins: [new MiniCssExtractPlugin({
+    filename: 'index.css'
+  })],
+  module: {
+      rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+      ],
+  },
+}
+
+module.exports = [config_js, config_css]
